db6730ca647fb3975e2dea7662860d76b3f10d5b
karnveer-thour <karanveer@devronins.com>
2026-02-26 15:50:10 +0530

    feat: introducing new enums

--- Backend/src/interface/common/common.enum.ts
+++ Backend/src/interface/common/common.enum.ts
@@ -20,3 +20,35 @@ export enum ELanguageLevel {
   advanced = 'advanced',
   native = 'native',
 }
+
+export enum EEducationType {
+  Matriculation = 'matriculation',
+  SeniorSecondary = 'seniorSecondary',
+  Graduation = 'graduation',
+  PostGraduation = 'postGraduation',
+  Doctorate = 'doctorate',
+}
+
+export enum EStream {
+  Humanities = 'humanities',
+  Commerce = 'commerce',
+  SciencePCM = 'sciencePCM',
+  SciencePCB = 'sciencePCB',
+  SciencePCMB = 'sciencePCMB',
+}
+
+export enum ESchoolBoard {
+  CBSE = 'cbse',
+  ICSE = 'icse',
+  ISC = 'isc',
+  NIOS = 'nios',
+  PSEB = 'pseb',
+  STATE_BOARD = 'stateBoard',
+  IB = 'ib',
+  CAMBRIDGE = 'cambridge',
+}
+
+export enum EGradeType {
+  CGPA = 'cgpa',
+  Percentage = '%',
+}
