8ac70daed4f4673a10b16a1d5266fc6e46f4ab25
K-Thour <karanveerthour76@gmail.com>
2026-04-03 22:55:53 +0530

    fix: edit profile button  in profile page issue fixed when user is unauthenticated

--- Portfolio-management-system/src/pages/Personal/Profile/components/ProfileHeader.tsx
+++ Portfolio-management-system/src/pages/Personal/Profile/components/ProfileHeader.tsx
@@ -2,10 +2,12 @@ import { ArrowBigLeft, Edit2 } from "lucide-react";
 import { useAppSelector } from "../../../../hooks/useRedux";
 import type { ProfileHeaderProps } from "../types";
 import { ThemeToggle } from "../../../../components/ui/themeToggle/ThemeToggle";
+import { useAuthentication } from "../../../../hooks/useAuthentication";
 
 export function ProfileHeader({ onEdit, onBack }: ProfileHeaderProps) {
   const { theme } = useAppSelector((state) => state.theme);
   const isDark = theme === "dark";
+  const { isAuthenticated } = useAuthentication();
 
   return (
     <div className="flex items-center justify-between mx-4 mt-3">
@@ -20,7 +22,7 @@ export function ProfileHeader({ onEdit, onBack }: ProfileHeaderProps) {
         <ArrowBigLeft className="w-5 h-5" />
         Back
       </button>
-      <div className="text-center ml-30">
+      <div className={`text-center ${isAuthenticated && "ml-30"}`}>
         <h1
           className={`text-3xl font-bold mb-2 ${isDark ? "text-white" : "text-gray-900"}`}
         >
@@ -34,17 +36,19 @@ export function ProfileHeader({ onEdit, onBack }: ProfileHeaderProps) {
       </div>
       <div className="flex items-center gap-16">
         <ThemeToggle />
-        <button
-          onClick={onEdit}
-          className={`flex items-center gap-2 px-6 py-3 rounded-xl font-medium transition-all hover:scale-105 ${
-            isDark
-              ? "bg-linear-to-r from-red-600 to-yellow-500 text-white hover:shadow-lg hover:shadow-red-500/50"
-              : "bg-linear-to-r from-blue-600 to-blue-500 text-white hover:shadow-lg hover:shadow-blue-500/50"
-          }`}
-        >
-          <Edit2 className="w-5 h-5" />
-          Edit Profile
-        </button>
+        {isAuthenticated && (
+          <button
+            onClick={onEdit}
+            className={`flex items-center gap-2 px-6 py-3 rounded-xl font-medium transition-all hover:scale-105 ${
+              isDark
+                ? "bg-linear-to-r from-red-600 to-yellow-500 text-white hover:shadow-lg hover:shadow-red-500/50"
+                : "bg-linear-to-r from-blue-600 to-blue-500 text-white hover:shadow-lg hover:shadow-blue-500/50"
+            }`}
+          >
+            <Edit2 className="w-5 h-5" />
+            Edit Profile
+          </button>
+        )}
       </div>
     </div>
   );
