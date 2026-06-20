1c2f96f2512c6b1c8697ca93b1e1abb7157524ff
K-Thour <karanveerthour76@gmail.com>
2026-06-20 21:52:19 +0530

    fix(backend): merge filter array in queryBuilder to fix Mongoose find parameter error

--- Backend/src/common/queryBuilder.ts
+++ Backend/src/common/queryBuilder.ts
@@ -4,7 +4,11 @@ const queryBuilder = <T>(queryParams: IqueryBuilderParams<T>) => {
   const { model, params } = queryParams;
   const { populate, select, filter = {}, sort = {} } = params || {};
 
-  let query = model.find(filter);
+  const parsedFilter = Array.isArray(filter)
+    ? filter.reduce((acc, curr) => ({ ...acc, ...curr }), {})
+    : filter;
+
+  let query = model.find(parsedFilter);
 
   if (select && select.length > 0) query = query.select(select);
   if (populate) query = query.populate(populate);
