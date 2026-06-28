35f5cf7b183c84a4df7b67e0cd0f22b54535430f
K-Thour <karanveerthour76@gmail.com>
2026-06-28 21:29:35 +0530

    fix: cors issue

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -15,16 +15,15 @@ app.use(
     limit: '50mb',
   }),
 );
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      callback(null, true);
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }),
-);
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  credentials: false
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
